eea9c638f2d1acc558fe72d10d98e6b436a8d487
aubrown <aubrown@matthew.cse.taylor.edu>
2009-09-26 22:20:06 -0400

    Added a template for creating a team and a basic .js file

--- intramurals/intramuralsApp/views.py
+++ intramurals/intramuralsApp/views.py
@@ -1,3 +1,7 @@
 # Create your views here.
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
-
+def dish_out_template(request, file_name):
+	return render_to_response(file_name)
+	
