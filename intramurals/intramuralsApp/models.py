cf89b4f3c30bc3cf8812d321764036f095c9e294
aubrown <aubrown@matthew.cse.taylor.edu>
2009-09-26 20:07:52 -0400

    Adding Thor's html files (html, png, css)

--- intramurals/intramuralsApp/models.py
+++ intramurals/intramuralsApp/models.py
@@ -12,16 +12,19 @@ class Person(models.Model):
 	shirt_size = models.CharField(max_length = 30)
 	campus_address = models.TextField()
 
-class League(models.Model):
+class Sport(models.Model):
+	sport_name = models.CharField(max_length = 50)
+	rules = models.TextField()
 
+class League(models.Model):
 	GENDER_CHOICES = (
 		('M', 'Male'),
 		('F', 'Female'),
 		('B', 'Co-ed'),
 	)
-
 	league_name = models.CharField(max_length = 50)
 	gender = models.CharField(max_length = 1, choices=GENDER_CHOICES)
+	sport = models.ForeignKey(Sport)
 
 class Team(models.Model):
 	team_name = models.CharField(max_length = 50)
@@ -30,6 +33,8 @@ class Team(models.Model):
 	league = models.ForeignKey(League)
 	living_unit = models.CharField(max_length = 50)
 		
+class StartEndTime(models.Model):
+	StartTime = models.DateTimeField()
+	EndTime = models.DateTimeField()
 
-
-
+	
