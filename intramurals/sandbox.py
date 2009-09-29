5fe1d9bae627ddc43392795fed944f3db096350e
rlong <rlong@matthew.cse.taylor.edu>
2009-09-28 20:43:25 -0400

    Remove the sandbox.py file from revision control.

--- intramurals/sandbox.py
+++
@@ -1,8 +0,0 @@
-
-# This is the database hosting information for the django projects.  Keep yours separate.  It is read from this file for each individual user
-DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-DATABASE_NAME = 'cos371-aubrown'             # Or path to database file if using sqlite3.
-DATABASE_USER = 'aubrown'             # Not used with sqlite3.
-DATABASE_PASSWORD = 'eizeemei'         # Not used with sqlite3.
-DATABASE_HOST = 'mysql.cse.taylor.edu'             # Set to empty string for localhost. Not used with sqlite3.
-DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
