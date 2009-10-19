ce877a255fbddb339cf57e6b54fe9ea2f42ad3fc
dlaskows <dlaskows@matthew.cse.taylor.edu>
2009-10-19 19:08:09 -0400

    some database work

--- intramurals/intramuralsApp/urls.py
+++ intramurals/intramuralsApp/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from intramuralsApp.views import dish_out_template
 urlpatterns = patterns('',
+    url('^mynameis/(.*)', 'intramuralsApp.views.say_hi'),
     url('^templates/(.*)', dish_out_template),
     url('^$', 'intramuralsApp.views.index'),
 )
