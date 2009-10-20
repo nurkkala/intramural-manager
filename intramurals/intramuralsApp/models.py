44a79b7fb721c4cf09ac61ea4d8855bbceb2fe82
rlong <rlong@matthew.cse.taylor.edu>
2009-10-19 20:44:11 -0400

    Added Divisions table

---
+++ intramurals/intramuralsApp/models.py
@@ -0,0 +1,55 @@
+from django.db import models
+from django.contrib.localflavor.us.models import PhoneNumberField
+
+class Persons(models.Model):
+	StudentID = models.PositiveIntegerField()
+	FirstName = models.CharField(max_length = 30)
+	LastName = models.CharField(max_length = 30)
+	Email = models.EmailField()
+	PhoneNumber = PhoneNumberField()
+	ShirtSize = models.CharField(max_length = 30)
+	Address = models.TextField()
+
+class Sports(models.Model):
+	Sport_Name = models.CharField(max_length = 50)
+	rules = models.TextField()
+
+class Attributes(models.Model):
+	Name = models.TextField()
+	Value = models.TextField()
+
+class Leagues(models.Model):
+	Attributes = models.ManyToManyField(Attribute)
+	LeagueName = models.CharField(max_length = 50)
+
+class Teams(models.Model):
+	Members = models.ManyToManyField(Person)
+	TeamName = models.CharField(max_length = 50)
+	Password = models.CharField(max_length = 50)
+	CaptainID = models.ForeignKey(Person)
+	LeagueID = models.ForeignKey(League)
+	LivingUnit = models.CharField(max_length = 50)
+
+class Locations(models.Model):
+	LocationName = models.CharField(max_length = 50)
+	LocationDescription = models.TextField();
+
+class Games(models.Model):
+	StartTime = models.DateTimeField()
+	Location = models.ForeignKey(Location)
+	GameType = models.TextField()
+	HomeTeamID = models.ForeignKey(Team)
+	AwayTeamID = models.ForeignKey(Team)
+	HomeTeamScore = models.PositiveIntegerField()
+	AwayTeamScore = models.PositiveIntegerField()
+	
+class Admins(models.Model):
+	UserName = models.CharField(max_length = 50)
+	Password = models.CharField(max_length = 50)
+
+class Referees(models.Model):
+	PersonID = models.ForeignKey(Person)
+	AttributeGroupID = models.ForeignKey(AttributeGroup)
+	
+class Divisions(models.Model):
+	DivisionName = models.TestField()
