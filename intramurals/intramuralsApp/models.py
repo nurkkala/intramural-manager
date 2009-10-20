598c6926ac86771996b4273bfeb892972fa270ca
dlaskows <dlaskows@matthew.cse.taylor.edu>
2009-10-19 20:18:02 -0400

    Added referee

--- intramurals/intramuralsApp/models.py
+++ intramurals/intramuralsApp/models.py
@@ -2,40 +2,48 @@ from django.db import models
 from django.contrib.localflavor.us.models import PhoneNumberField
 
 class Person(models.Model):
-	student_id = models.PositiveIntegerField()
-	first_name = models.CharField(max_length = 30)
-	last_name = models.CharField(max_length = 30)
-	email = models.EmailField()
-	phone_number = PhoneNumberField()
-	shirt_size = models.CharField(max_length = 30)
-	campus_address = models.TextField()
+	StudentID = models.PositiveIntegerField()
+	FirstName = models.CharField(max_length = 30)
+	LastName = models.CharField(max_length = 30)
+	Email = models.EmailField()
+	PhoneNumber = PhoneNumberField()
+	ShirtSize = models.CharField(max_length = 30)
+	Address = models.TextField()
 
 class Attribute(models.Model):
-	name = models.TextField()
-	value = models.TextField()
+	Name = models.TextField()
+	Value = models.TextField()
 
 class League(models.Model):
-	attributes = models.ManyToManyField(Attribute)
-	league_name = models.CharField(max_length = 50)
+	Attributes = models.ManyToManyField(Attribute)
+	LeagueName = models.CharField(max_length = 50)
 
 class Team(models.Model):
-	members = models.ManyToManyField(Person)
-	team_name = models.CharField(max_length = 50)
-	password = models.CharField(max_length = 50)
-	captain = models.ForeignKey(Person)
-	league = models.ForeignKey(League)
-	living_unit = models.CharField(max_length = 50)
+	Members = models.ManyToManyField(Person)
+	TeamName = models.CharField(max_length = 50)
+	Password = models.CharField(max_length = 50)
+	CaptainID = models.ForeignKey(Person)
+	LeagueID = models.ForeignKey(League)
+	LivingUnit = models.CharField(max_length = 50)
 
 class Location(models.Model):
-	location_name = models.CharField(max_length = 50)
-	location_description = models.TextField();
+	LocationName = models.CharField(max_length = 50)
+	LocationDescription = models.TextField();
 
 class Game(models.Model):
-	start_time = models.DateTimeField()
-	location = models.ForeignKey(Location)
-	game_type = models.TextField()
-	home_team = models.ForeignKey(Team)
-	away_team = models.ForeignKey(Team)
-	home_team_score = models.PositiveIntegerField()
-	away_team_score = models.PositiveIntegerField()
+	StartTime = models.DateTimeField()
+	Location = models.ForeignKey(Location)
+	GameType = models.TextField()
+	HomeTeamID = models.ForeignKey(Team)
+	AwayTeamID = models.ForeignKey(Team)
+	HomeTeamScore = models.PositiveIntegerField()
+	AwayTeamScore = models.PositiveIntegerField()
 	
+class Admin(models.Model):
+	UserName = models.CharField(max_length = 50)
+	Password = models.CharField(max_length = 50)
+
+class Referee(models.Model):
+	 PersonID = models.ForeignKey(Person)
+	 AttributeGroupID = models.ForeignKey(AttributeGroup)
+	 
