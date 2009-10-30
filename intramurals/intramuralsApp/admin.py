7c097c9b2d0169fa7dd0b769c0fe6d1c592fb404
mtamny <mtamny@matthew.cse.taylor.edu>
2009-10-29 21:32:13 -0400

    added admin.py

---
+++ intramurals/intramuralsApp/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from intramurals.intramuralsApp.models import *
+
+admin.site.register(Person)
+admin.site.register(Attribute)
+admin.site.register(AttributeGroup)
+admin.site.register(Division)
+admin.site.register(Team)
+admin.site.register(Referee)
+admin.site.register(Season)
+admin.site.register(League)
+admin.site.register(Sport)
+admin.site.register(Location)
+admin.site.register(Game)
