49238dc2a0432ea8a1453aa61d4e755a1d80863e
dlaskows <dlaskows@localhost>
2009-10-30 16:06:51 -0400

    Indented refs and fixed pop.sql

--- intramurals/intramuralsApp/populateDB.sql
+++ intramurals/intramuralsApp/populateDB.sql
@@ -1,9 +1,9 @@
 /* Persons */
-INSERT INTO `intramuralsApp_persons` (`id`, `StudentID`, `FirstName`, `LastName`, `Email`, `PhoneNumber`, `ShirtSize`, `Address`) VALUES(1, 123456, 'Bogus', 'Dude', 'bogusdude@gmail.com', '123-456-7890', 'XL', 'Bergwall Hall 123');
+INSERT INTO `intramuralsApp_person` (`id`, `StudentID`, `FirstName`, `LastName`, `Email`, `PhoneNumber`, `ShirtSize`, `Address`) VALUES(1, 123456, 'Bogus', 'Dude', 'bogusdude@gmail.com', '123-456-7890', 'XL', 'Bergwall Hall 123');
 
 
 /* Sports */
-INSERT INTO `cos371-dlaskows`.`intramuralsApp_sports` (
+INSERT INTO `intramuralsApp_sport` (
 `id` ,
 `SportName` ,
 `SportRules`
@@ -12,7 +12,7 @@ VALUES (
 '1', 'Basketball', 'Throw the ball through the hoop.'
 );
 
-INSERT INTO `cos371-dlaskows`.`intramuralsApp_sports` (
+INSERT INTO `intramuralsApp_sport` (
 `id` ,
 `SportName` ,
 `SportRules`
@@ -21,7 +21,7 @@ VALUES (
 '2', 'Football', 'Grab ball and run.'
 );
 
-INSERT INTO `cos371-dlaskows`.`intramuralsApp_sports` (
+INSERT INTO `intramuralsApp_sport` (
 `id` ,
 `SportName` ,
 `SportRules`
@@ -30,7 +30,7 @@ VALUES (
 '3', 'Soccer', 'Kick ball into goal.'
 );
 
-INSERT INTO `cos371-dlaskows`.`intramuralsApp_sports` (
+INSERT INTO `intramuralsApp_sport` (
 `id` ,
 `SportName` ,
 `SportRules`
@@ -42,7 +42,7 @@ VALUES (
 
 
 /* Seasons */
-INSERT INTO `cos371-dlaskows`.`intramuralsApp_seasons` (
+INSERT INTO `intramuralsApp_season` (
 `id` ,
 `SeasonStart` ,
 `SeasonName` ,
@@ -53,7 +53,7 @@ VALUES (
 '1', '2009-09-21 17:53:18', 'Fall Football 2009-2010', '2009-09-08 17:52:24', '2009-09-15 17:52:41'
 );
 
-INSERT INTO `cos371-dlaskows`.`intramuralsApp_seasons` (
+INSERT INTO `intramuralsApp_season` (
 `id` ,
 `SeasonStart` ,
 `SeasonName` ,
@@ -64,7 +64,7 @@ VALUES (
 '2', '2009-09-21 17:53:18', 'Soccer 2009-2010', '2009-09-08 17:52:24', '2009-09-15 17:52:41'
 );
 
-INSERT INTO `cos371-dlaskows`.`intramuralsApp_seasons` (
+INSERT INTO `intramuralsApp_season` (
 `id` ,
 `SeasonStart` ,
 `SeasonName` ,
@@ -78,10 +78,10 @@ VALUES (
 /* Sports to Seasons */
 
 /* Referees */
-INSERT INTO `intramuralsApp_referees` (`id`, `Person_id`, `Attribute_id`) VALUES(1, 1, 1);
+INSERT INTO `intramuralsApp_referee` (`id`, `Person_id`, `Attribute_id`) VALUES(1, 1, 1);
 
 /* Attributes */
-INSERT INTO `intramuralsApp_attributes` VALUES(1, 1, 'Soccer');
+INSERT INTO `intramuralsApp_attribute` VALUES(1, 1, 'Soccer');
 
 /* AttributeGroups */
-INSERT INTO `intramuralsApp_attributegroups` (`id`, `AttributeGroupName`, `AttributeGroupDescription`) VALUES(1, 'Level', 'The level at which the referee is authorized to referee');
+INSERT INTO `intramuralsApp_attributegroup` (`id`, `AttributeGroupName`, `AttributeGroupDescription`) VALUES(1, 'Level', 'The level at which the referee is authorized to referee');
