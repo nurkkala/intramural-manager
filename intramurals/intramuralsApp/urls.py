e6feb6ef03e0c088d6155eaa851fe4dbce023fba
dlaskows <dlaskows@matthew.cse.taylor.edu>
2009-11-02 20:54:54 -0500

    Working on templates and views; it's in an unstable state right now.

--- intramurals/intramuralsApp/urls.py
+++ intramurals/intramuralsApp/urls.py
@@ -2,6 +2,12 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('intramuralsApp.views',
     url('^mynameis/(.*)', 'say_hi'),
     url('^$', 'index'),
-    url('^referees/$', 'referees'),
+    url('^schedule$', 'schedule'),
+    url('^sports$', 'sports'),
+    url('^register$', 'register'),
+    url('^standings$', 'standings'),
+    url('^referees$', 'referees'),
+    url('^about$', 'about'),
+    url('^admin$', 'admin'),
     url('^(.*)', 'dish_out_template'),
 )
