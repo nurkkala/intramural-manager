1c0eb0c1ecd9597174700a55f394cc2ffed8de49
aubrown <aubrown@matthew.cse.taylor.edu>
2009-11-05 21:21:49 -0500

    Added some Referee Schedule funcionality and form functionality for the createTeam.html template.

---
+++ intramurals/intramuralsApp/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls.defaults import *
+urlpatterns = patterns('intramuralsApp.views',
+    url('^mynameis/(.*)', 'say_hi'),
+    url(r'^$', 'index'),
+    url(r'^home$', 'index'),
+    url(r'^schedule/$', 'schedule'),
+    url('^sports$', 'sports'),
+    url('^registerTeam', 'registerTeam'),
+    url('^standings$', 'standings'),
+    url('^referees/(\d+)', 'refereeSchedule'),
+    url('^referees$', 'referees'),
+    url('^about$', 'about'),
+    url('^admin$', 'admin'),
+    url('^(.*)', 'index'),
+)
---
+++ intramurals/intramuralsApp/views.py
@@ -0,0 +1,83 @@
+# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import Template, Context
+from django.http import HttpResponse
+from models import *
+from forms import *
+
+def index(request):
+    return render_to_response("home.html")
+
+def say_hi(request, name):
+    t = Template("<html><body>Well hi there {{ name_of_person }}!</body></html>")
+    c = Context({'name_of_person': name})
+    html = t.render(c)
+    return HttpResponse(html)
+
+def dish_out_template(request, file_name):
+    return render_to_response(file_name)
+
+def schedule(request):
+    return render_to_response("schedule.html", locals())
+
+def sports(request):
+    sportList = Sport.objects.all()
+    return render_to_response("sports.html", locals())
+
+# I Need To Get The
+# hard-coded copies to be
+# created dynamically!!!!
+def registerTeam(request):
+    form = CreateTeamForm(request.POST)
+    if request.method == 'POST':
+        if form.is_valid():
+            if request.POST['teamPassword'] == request.POST["repeatTeamPassword"]:
+                cd = form.cleaned_data
+                division = Division.objects.get(DivisionName = "Unassigned")
+                captain = Person(StudentID=cd['captainId'], FirstName=cd['captainFirstName'], LastName=cd['captainLastName'], Email=cd['captainEmail'], ShirtSize="XXL", Address="236 W. Reade Ave.")
+                captain.save()
+                team = Team(TeamName=cd['teamName'], Password=cd['teamPassword'], Captain=captain, Division = division, LivingUnit="Sammy II")
+                team.save()
+                return render_to_response("congrats.html", locals())
+
+            else: 
+                return render_to_response("createTeam.html", {"pword_error":True})
+        else:   
+            return render_to_response("createTeam.html", locals())
+    else:
+        return render_to_response("createTeam.html")
+ 
+def standings(request):
+    return render_to_response("standings.html", locals())
+
+def referees(request):
+    sportList = Sport.objects.all()
+    for sport in sportList:
+        sport.seasonList = sport.season_set.all()
+        for season in sport.seasonList:
+            season.leagueList = season.league_set.all()
+            for league in season.leagueList:
+                league.refereeList = league.Referees.all()
+    return render_to_response("referees.html", locals())
+
+def about(request):
+    return render_to_response("about.html", locals())
+
+def admin(request):
+    return render_to_response("admin.html", locals())
+
+def refereeSchedule(request, refId):
+    referee = Referee.objects.get(id=refId)
+    gameList = referee.game_set.all()
+   # for game in gameList:
+   #     game.sport = teamToSport(game.HomeTeam)
+    return render_to_response("refereeSchedule.html", locals())
+
+
+
+def teamToSport(teamName):
+    team = Team.objects.get(TeamName=teamName)
+    sport = team.division.league.season.sport
+    return sport
+
+
