e472313c50e750cff6a630bd3db8a02fc66b7f05
dlaskows <dlaskows@matthew.cse.taylor.edu>
2009-11-20 16:32:54 -0500

    Merging

--- intramurals/intramuralsApp/urls.py
+++ intramurals/intramuralsApp/urls.py
@@ -9,11 +9,11 @@ urlpatterns = patterns('intramuralsApp.views',
     url('^createTeam', 'createTeam'),
     url('^joinTeam', 'joinTeam'),
     url('^teams/(\d+)', 'teamHomepage'),# the home page for team of given id
-    url('^standings/(.+)', 'standings'),# view standings for a specific sport
-    url('^standings$', 'standings'),# view standings for all sports
+    url('^standings/(.+)', 'standingsOneSport'),# view standings for a specific sport
+    url('^standings$', 'standingsAllSports'),# view standings for all sports
     url('^referees/(\d+)', 'refereeSchedule'),# the schedule page for referee of given id
-    url('^referees/(.+)', 'referees'),# view referees for a specific sport
-    url('^referees$', 'referees'),# view referees for all sports
+    url('^referees/(.+)', 'refereesOneSport'),# view referees for a specific sport
+    url('^referees$', 'refereesAllSports'),# view referees for all sports
     url('^about$', 'about'),
     url('^admin$', 'admin'),
 )
