9d774256040cabbd23a4b7e6da8d3000c4025ba5
sbird <sbird@matthew.cse.taylor.edu>
2010-01-22 01:41:21 -0500

    temp commit

--- intramurals/intramuralsApp/schedule.py
+++ intramurals/intramuralsApp/schedule.py
@@ -11,3 +11,13 @@ from django.core import serializers
 
 def daySched(request):
     return HttpResponse("testing")
+
+def getGames(request):
+    if(not request.GET or request.GET['start'] == "" or request.GET['end'] == ""):
+        return HttpResponse('"start" and "end" get values need to be passed')
+    json_serializer = serializers.get_serializer("json")()
+    o = Game.objects.select_related(depth=1).extra(where=["%s <= date(StartTime) and date(StartTime) <= %s"], params=[request.GET['start'], request.GET['end']])
+    json_serializer.serialize(o, relations=('HomeTeam', 'AwayTeam',))
+    val = json_serializer.getvalue()
+    return HttpResponse(val);
+
