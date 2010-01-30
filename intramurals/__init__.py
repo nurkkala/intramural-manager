0b6842e08cf2ab83abeb3c3f88fd81b5d99425c9
cos372f0901 <cos372f0901@matthew.cse.taylor.edu>
2010-01-30 13:47:56 -0500

    Temporary commit to save progress while Austin and Seth try to get the Admin site working

--- intramurals/__init__.py
+++ intramurals/__init__.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render_to_response
+from django.http import HttpResponse
 
+def foo(request):
+	return HttpResponse("foo")
 
 def dish_out_template(request, file_name):
 	return render_to_response(file_name)
