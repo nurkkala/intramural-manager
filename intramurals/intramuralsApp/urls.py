230afa34cce11eaeff9d1d7a3c364d50953f6f4f
cos372f0901 <cos372f0901@matthew.cse.taylor.edu>
2010-01-30 16:44:16 -0500

    fixed admin functionality. This was when Austin and Seth spent 2.45 eons in his room.

--- intramurals/intramuralsApp/urls.py
+++ intramurals/intramuralsApp/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('intramuralsApp.views',
     url('^createTeam2', 'createTeam2'),
     url('^joinTeam1', 'joinTeam1'),
     url('^joinTeam2', 'joinTeam2'),
-    url('^joinTeam3', 'joinTeam3'),
+##    url('^joinTeam3', 'joinTeam3'),  TODO: actually write this view. until then, don't uncomment or the admin/ functionality will break
 
     url('^schedule$','daySched'),
     url('^schedule/(.+)$','daySched'),# day specified
@@ -17,8 +17,8 @@ urlpatterns = patterns('intramuralsApp.views',
 
     url('^referees/(\d+)$', 'refereeSchedule'),# the schedule page for referee of given id
     url('^teams/(\d+)', 'teamHomepage'),       # the home page for team of given id
-    url('^about$', 'about'),
-    url('^admin$', 'admin'),
+##    url('^about$', 'about'),  TODO: actually write this view. until then, don't uncomment or the admin/ functionality will break
+##    url('^admin$', 'admin'),  TODO: actually write this view. until then, don't uncomment or the admin/ functionality will break
 
     url('^daySched','daySched'),
     url('^getGames', 'getGames'),
