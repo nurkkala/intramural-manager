ab513d61056d652393e01e48d9d3c981b83b4d28
cos372f0901 <cos372f0901@matthew.cse.taylor.edu>
2010-01-30 16:44:16 -0500

    fixed admin functionality. This was when Austin and Seth spent 2.45 eons in his room.

--- intramurals/urls.py
+++ intramurals/urls.py
@@ -2,12 +2,10 @@ from django.conf.urls.defaults import *
 from __init__ import dish_out_template
 
 from django.contrib import admin
-#admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url('templates/(.*)', dish_out_template), #TODO: delete this for production (security hole)
-    (r'^admin/(.*)', include(admin.site.urls)),
-#    (r'^admin/(.*)', admin.site.root),
-    (r'', include('intramuralsApp.urls')),
-    (r'', include('jcal.urls')),
+    (r'^admin/', include(admin.site.urls)),
+    (r'asdf', include('intramuralsApp.urls')),
 )
