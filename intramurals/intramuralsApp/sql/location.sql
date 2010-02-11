589b0eb66818250b25e64f9430fcd9f2d89629a8
aubrown <aubrown@matthew.cse.taylor.edu>
2010-02-11 01:34:41 -0500

    Added sql files for each table (roughly speaking)

---
+++ intramurals/intramuralsApp/sql/location.sql
@@ -0,0 +1,31 @@
+/* Locations */
+INSERT INTO `intramuralsApp_location` VALUES
+(1, 'Field 1', 'Eastmost intramural football field on southeast corner of campus', 1),
+(2, 'Field 2', 'Center intramural football field on southeast corner of campus', 1),
+(3, 'Field 3', 'Westmost intramural football field on southeast corner of campus', 1),
+(4, 'Court 1', 'Tennis courts closest to Rediger Auditorium', 2),
+(5, 'Court 2', 'Tennis courts 2nd closest to Rediger Auditorium', 2),
+(6, 'Court 3', 'Tennis courts 3rd closest to Rediger Auditorium', 2),
+(7, 'Court 4', 'Tennis courts 4th closest to Rediger Auditorium', 2),
+(8, 'Field 1', 'Soccer field closest to campus', 3),
+(9, 'Field 2', 'Soccer field 2nd closest to campus', 3),
+(10, 'Field 3', 'Soccer field 2nd furthest from campus', 3),
+(11, 'Field 4', 'Soccer field furthest from campus', 3),
+(12, 'Court 1', 'Basketball court in the KSAC closest to the lobby', 4),
+(13, 'Court 2', 'Basketball court in the KSAC 2nd closest to the lobby', 4),
+(14, 'Court 3', 'Basketball court in the KSAC 2nd furthest from the lobby', 4),
+(15, 'Court 4', 'Basketball court in the KSAC furthest from the lobby', 4);
+
+/* Location Groups */
+INSERT INTO `intramuralsApp_locationgroup` VALUES
+(1, 'Football fields', ''),
+(2, 'Tennis courts', ''),
+(3, 'Soccer fields', ''),
+(4, 'KSAC courts', '');
+
+/* Location Groups to Sports */
+INSERT INTO `intramuralsApp_locationgroup_Sports` VALUES
+(1, 1, 1),
+(2, 2, 2),
+(3, 3, 3),
+(4, 4, 4);
