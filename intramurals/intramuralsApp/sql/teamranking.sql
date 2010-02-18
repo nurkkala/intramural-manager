cc7ec31501a728daf318f223ff98b0d7255de7e3
sbird <sbird@matthew.cse.taylor.edu>
2010-02-18 11:33:07 -0500

    trying to work on rankings.

--- intramurals/intramuralsApp/sql/teamranking.sql
+++ intramurals/intramuralsApp/sql/teamranking.sql
@@ -1,29 +1,32 @@
+
+--DROP PROCEDURE IF EXISTS intramuralsApp_max_wins;
+--create procedure intramuralsApp_max_wins()
+--       select max(wins) as max_wins 
+--       from intramuralsApp_wins;
+
 create or replace view intramuralsApp_teamranking as 
-       select count(name) as ties, T.id  
-       from intramuralsApp_team AS T join intramuralsApp_game AS G 
-       where (T.id = HomeTeam_id and HomeTeamScore = AwayTeamScore) or (T.id = AwayTeam_id and AwayTeamScore = HomeTeamScore) 
-       GROUP BY id;
+       select W.id, W.wins as wins, L.losses as losses, T.ties as ties, W.wins - L.losses + W.wins / 1000 as rank
+       from intramuralsApp_wins as W
+       	    join intramuralsApp_losses as L on W.id = L.id
+	    join intramuralsApp_ties as T on T.id = W.id
+       order by rank desc;
+
 
 create or replace view intramuralsApp_ties as 
-       select count(name) as ties, T.id  
-       from intramuralsApp_team AS T join intramuralsApp_game AS G 
+       select count(T.id) as ties, T.id  
+       from intramuralsApp_team AS T join intramuralsApp_game AS G
        where (T.id = HomeTeam_id and HomeTeamScore = AwayTeamScore) or (T.id = AwayTeam_id and AwayTeamScore = HomeTeamScore) 
        GROUP BY id;
 
 create or replace view intramuralsApp_wins as 
-       select count(name) as wins, T.id  
-       from intramuralsApp_team AS T join intramuralsApp_game AS G 
-       where (T.id = HomeTeam_id and HomeTeamScore > AwayTeamScore) or (T.id = AwayTeam_id and AwayTeamScore > HomeTeamScore) 
+       select count(id) as wins, T.id  
+       from intramuralsApp_team AS T left join intramuralsApp_game AS G on (T.id = HomeTeam_id and HomeTeamScore > AwayTeamScore) or (T.id = AwayTeam_id and AwayTeamScore > HomeTeamScore) 
        GROUP BY id;
 
 create or replace view intramuralsApp_losses as 
-       select count(name) as losses, name, T.id  
+       select count(id) as losses, T.id  
        from intramuralsApp_team AS T join intramuralsApp_game AS G 
        where (T.id = HomeTeam_id and HomeTeamScore < AwayTeamScore) or (T.id = AwayTeam_id and AwayTeamScore < HomeTeamScore) 
        GROUP BY id;
 
 
-DROP PROCEDURE IF EXISTS `max_wins`;
-create procedure max_wins()  
-       select max(wins) as max_wins 
-       from intramuralsApp_wins;
