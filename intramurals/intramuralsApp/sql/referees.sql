dcfacf343457abc7927a0556704f6e14b4262190
sbird <sbird@cse.taylor.edu>
2010-04-10 23:44:24 +0000

    - unstable - in middle of fixing mysql comments

--- intramurals/intramuralsApp/sql/referees.sql
+++ intramurals/intramuralsApp/sql/referees.sql
@@ -1,32 +1,29 @@
 
-/* Referees 
-INSERT INTO `intramuralsApp_referee` VALUES
-(1, 1, 1),
-(2, 2, 1),
-(3, 3, 1);
-*/
+/* Referees */
+--INSERT INTO `intramuralsApp_referee` VALUES
+--(1, 1, 1),
+--(2, 2, 1),
+--(3, 3, 1);
 
-/* Games to Referees 
-INSERT INTO `intramuralsApp_game_Referees` VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 2, 1),
-(4, 2, 2),
-(5, 3, 1),
-(6, 4, 1),
-(7, 5, 3),
-(8, 6, 3),
-(9, 7, 2),
-(10, 8, 2),
-(11, 8, 3);
-*/
+/* Games to Referees */
+--INSERT INTO `intramuralsApp_game_Referees` VALUES
+--(1, 1, 1),
+--(2, 1, 2),
+--(3, 2, 1),
+--(4, 2, 2),
+--(5, 3, 1),
+--(6, 4, 1),
+--(7, 5, 3),
+--(8, 6, 3),
+--(9, 7, 2),
+--(10, 8, 2),
+--(11, 8, 3);
 
-/* Leagues to Referees 
-INSERT INTO `intramuralsApp_league_Referees` VALUES
-(1, 1, 1),
-(2, 1, 2),
-(3, 1, 3),
-(4, 2, 1),
-(5, 2, 2),
-(6, 3, 4);
-*/
+/* Leagues to Referees */
+--INSERT INTO `intramuralsApp_league_Referees` VALUES
+--(1, 1, 1),
+--(2, 1, 2),
+--(3, 1, 3),
+--(4, 2, 1),
+--(5, 2, 2),
+--(6, 3, 4);
